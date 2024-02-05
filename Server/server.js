125125ffaf825a8eaef4f3e6e222b5b25778c179
Kimsunghyun <kimsunghyunee@gmail.com>
2024-02-05 16:39:27 +0900

    re2

--- Server/server.js
+++ Server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use('/api', postRoutes);
 app.use('/api', getRoutes);
 app.use('/api', userRoutes);
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, './build')));
 
 
 app.get('/', function (req, res) { res.sendFile(path.join(__dirname, './build/index.html')); });
